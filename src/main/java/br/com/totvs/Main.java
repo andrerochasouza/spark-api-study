bf9c31a1a3370851b409fb6ea46bab8bfed7adfd
andre.rsouza <andre.rsouza@totvs.com.br>
2023-01-26 12:12:02 -0300

    Add new import Gson (convert RESPONSE HTML to JSON)

--- src/main/java/br/com/totvs/Main.java
+++ src/main/java/br/com/totvs/Main.java
@@ -1,8 +1,11 @@
 package br.com.totvs;
 
 import br.com.totvs.BD.SQLiteConnection;
+import br.com.totvs.Domain.Familia;
 import br.com.totvs.Repository.DespesaRepository;
 import br.com.totvs.Repository.FamiliaRepository;
+import com.google.gson.Gson;
+import org.eclipse.jetty.util.ajax.JSON;
 
 import static spark.Spark.*;
 
@@ -11,15 +14,24 @@ public class Main {
         port(4567);
         System.out.println("Servidor rodando na porta 4567");
 
+        initExceptionHandler((e) -> System.out.println("Erro ao iniciar o servidor."));
+
         SQLiteConnection.getInstance().testConnection();
         DespesaRepository despesaRepository = new DespesaRepository();
         FamiliaRepository familiaRepository = new FamiliaRepository();
 
-        get("/despesas", (req, res) -> despesaRepository.getAllDespesas());
-        get("/despesas/:id", (req, res) -> despesaRepository.getDespesa(Integer.parseInt(req.params(":id"))));
+        Gson gson = new Gson();
+
+
         post("/familia/add", (req, res) -> {
             familiaRepository.addFamilia(req.queryParams("nome"), Double.parseDouble(req.queryParams("salario")));
             return "Despesa inserida com sucesso.";
         });
+
+        get("/familia/:id", (req, res) -> {
+
+            int id = Integer.parseInt(req.params(":id"));
+            return familiaRepository.getFamilia(id);
+        }, gson::toJson);
     }
 }
